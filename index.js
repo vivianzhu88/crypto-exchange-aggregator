bd02c0f341109459cea6dde0c5862494a57c139e
Stephanie Yen <stephanieyen@Stephanies-MacBook-Air.local>
2022-07-28 19:07:07 -0400

    FTX API authentication - working on pulling and storing all FTX data

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-// user variables
-var axios = require('axios');
+// load JavaScript modules
+const axios = require('axios');
 const prompt = require('prompt-sync')();
+const crypto = require('crypto');
 
 // user variables
 // hard-coded for now
 baseCurrency = 'DAI'
-quoteCurrency = 'USDT' // default
-buy = true
+quoteCurrency = 'USDT'
 quantity = 10000
 
 matcha = function() {
@@ -34,24 +34,60 @@ matcha = function() {
 // matcha()
 
 
-// CEX order books
-ftx = function() {
-    config = {
-        method: 'get', 
-        url: `https://ftx.com/api/markets/${baseCurrency}/${quoteCurrency}/orderbook?depth=20`
+// CEX APIs ---------------------------------------------------------------------------------------------
+
+// base urls
+ftxBaseUrl = `https://ftx.us/api`
+
+async function ftx() {
+    let dict = {"exchange": "ftx", "bids": [], "asks": [], "gasFee":0.03, "exchangeFee":0, "withdrawalFee":0}
+
+    // https://blog.ftx.com/blog/api-authentication/    
+    const FTX_KEY = 'L4dEDrXVRTvQdlzx2BDKJbcYE1s0dDM2edLY8OBW'
+    const FTX_SECRET= 'ZJ4wI4O_n5wsywK0CiS9dj3KSPwb0p3RAE6NH_St'
+    
+    timestamp = Date.now()
+    // signaturePayload = `${timestamp}GET/api/fills?market=DAI/USDT`
+    signaturePayload = `${timestamp}GET/api/account`
+    signature = crypto.createHmac("sha256", encodeURI(FTX_SECRET)).update(encodeURI(signaturePayload)).digest('hex');
+
+    const config = {
+        method: 'get',
+        // url: `${ftxBaseUrl}/markets/DAI/USDT/orderbook?depth=20`,
+        url: `${ftxBaseUrl}/account`,
+
+        // https://stackoverflow.com/questions/69213825/ftx-get-account-info-using-python-gives-not-logged-in-error 
+        headers: {
+            'FTXUS-KEY': FTX_KEY,
+            'FTXUS-SIGN': signature,
+            'FTXUS-TS': timestamp.toString()
+        }
     }
-    axios(config)
-    .then(function (response) {
+
+    function getOrderbook(response) {
         orderbook = response.data["result"]
+
         bids = orderbook["bids"]
         asks = orderbook["asks"]
-        console.log(bids)
-        console.log(asks)
-    })
-    .catch(function (error) {
-        console.log(error["response"]["data"]["error"]);
-    });
+
+        return [bids, asks]
+    }
+
+    // const response = await axios.get(`https://ftx.com/api/markets/DAI/USDT/orderbook?depth=20`, config)
+    // return getOrderbook(response)
+
+    const response = await axios(config)
+    return response
 }
+
+ftx()
+.then(data => {
+    console.log(data)
+})
+.catch(err => 
+    // console.log(err["response"]["data"]["error"])
+    console.log(err)
+)
 
 kucoin = function() {
     config = {
@@ -100,7 +136,7 @@ kraken = function() {
 
 async function gemini() {
     // og format: { price: '20838.99', amount: '0.26479', timestamp: '1658861155' }
-    function getBidsAsks(response) {
+    function getOrderbook(response) {
         orderbook = response.data
 
         function bidAskReformat(obj) {
@@ -118,75 +154,83 @@ async function gemini() {
     }
 
     const response = await axios.get(`https://api.gemini.com/v1/book/btcusd`)
-    return getBidsAsks(response)
+
+    // get orderbook (bids/asks)
+    // get fees
+    return getOrderbook(response)
 }
 
-// ftx()
 // kucoin()
 // kraken()
 // gemini()
 
-// we will eventually need a dictionary to store all the prices that we calculate
-// var dict = {'gemini': []}
+let dict = {}
 
-gemini()
-.then(data => {
-    // bids = data[0]
-    // asks = data[1]
-    // based on the orderbook (bids/asks), invoke a callback function 
-    // to calculate the best execution price
-})
-.catch(err => console.log(err))
+// gemini()
+// .then(data => {
+//     // bids = data[0]
+//     // asks = data[1]
+//     // based on the orderbook (bids/asks), invoke a callback function 
+//     // to calculate the best execution price
+//     console.log(data)
+// })
+// .catch(err => console.log(err))
 
+// 'orderbook' is a dictionary
+// ['bids':[[Array], [Array]], 'asks': [[Array], [Array]]]
+// where Array = [price, amount] <-- both in number format
+function getPriceFromOrderbook(orderbook) {
+
+}
+
+// {'exchange1': price, 'exchange2': price}
 async function get_prices(i_ticker, f_ticker, i_amount){
 
 }
 
-//main method ---------------------------------------------------------------------------------------------
-var init_ticker = prompt("Ticker of the token you have: ");
-var final_ticker = prompt("Ticker of the token you want to trade into: ");
-var init_amount = prompt("How much " + init_ticker + " do you want to trade? ");
-console.log("\nConverting " + init_amount + " " + init_ticker + " to " + final_ticker);
+// main method ---------------------------------------------------------------------------------------------
+function main() {
+    var init_ticker = prompt("Ticker of the token you have: ");
+    var final_ticker = prompt("Ticker of the token you want to trade into: ");
+    var init_amount = prompt("How much " + init_ticker + " do you want to trade? ");
+    console.log("\nConverting " + init_amount + " " + init_ticker + " to " + final_ticker);
 
-//get prices from all exchanges
-//each list inside main list represents a path
-//format for ouput is [total price init/final ticker, [exchange, # init token to trade], [exchange, # init token to trade, intermediary token ticker, # intermediary token to trade]]
-output = [
-    [200 , ["Matcha" , 5]],
-    [543291 , ["Kucoin" , 2], ["FTX" , 4], ["Matcha" , 1]],
-    [100000 , ["FTX" , 1 , "USDT", 5 ]]
-];
-//output = get_prices(init_ticker, final_ticker, init_amount);
+    //get prices from all exchanges
+    //each list inside main list represents a path
+    //format for ouput is [total price init/final ticker, [exchange, # init token to trade], [exchange, # init token to trade, intermediary token ticker, # intermediary token to trade]]
+    output = [
+        [200 , ["Matcha" , 5]],
+        [543291 , ["Kucoin" , 2], ["FTX" , 4], ["Matcha" , 1]],
+        [100000 , ["FTX" , 1 , "USDT", 5 ]]
+    ];
+    //output = get_prices(init_ticker, final_ticker, init_amount);
 
-//bubble sort algorithm lowest price -> highest price
-for (var i = 0; i < output.length-1; i++){
-    for (var j = 0, swapping; j < output.length-1; j++){
-        if (output[j][0] > output[j+1][0]){
-            swapping = output[j+1];
-            output[j+1] = output[j];
-            output[j] = swapping;
+    //bubble sort algorithm lowest price -> highest price
+    for (var i = 0; i < output.length-1; i++){
+        for (var j = 0, swapping; j < output.length-1; j++){
+            if (output[j][0] > output[j+1][0]){
+                swapping = output[j+1];
+                output[j+1] = output[j];
+                output[j] = swapping;
+            }
         }
     }
-}
 
-var output_string;
-for (let i = 0; i < output.length; i++) { //i is path
-    price = output[i][0];
-    output_string = "____________________________\n" + (i+1) + ". PRICE: " + price + " (" + init_ticker + "/" + final_ticker + ")\n\n";
-    for (let j = 1; j < output[i].length; j++) { //j is # exchanges - 1
-        exchange = output[i][j];
-        if (exchange.length > 2){ //there is intermediary token
-            output_string += "  - <" + exchange[0] + "> " + exchange[1] + " " + init_ticker + " to " + exchange[2] + ", " + exchange[3] + " " + exchange[2] +  " to " + final_ticker + "\n";
+    var output_string;
+    for (let i = 0; i < output.length; i++) { //i is path
+        price = output[i][0];
+        output_string = "____________________________\n" + (i+1) + ". PRICE: " + price + " (" + init_ticker + "/" + final_ticker + ")\n\n";
+        for (let j = 1; j < output[i].length; j++) { //j is # exchanges - 1
+            exchange = output[i][j];
+            if (exchange.length > 2){ //there is intermediary token
+                output_string += "  - <" + exchange[0] + "> " + exchange[1] + " " + init_ticker + " to " + exchange[2] + ", " + exchange[3] + " " + exchange[2] +  " to " + final_ticker + "\n";
+            }
+            else{
+                output_string += "  - <" + exchange[0] + "> " + exchange[1] + " " + init_ticker + " to " + final_ticker + "\n";
+            }
         }
-        else{
-            output_string += "  - <" + exchange[0] + "> " + exchange[1] + " " + init_ticker + " to " + final_ticker + "\n";
-        }
+        console.log(output_string);
     }
-    console.log(output_string);
 }
-
-
-
-
 
 
